25ac144bddff5c07a8f693ee4573835b98f9f139
DenSonmez <113019821+DenSonmez@users.noreply.github.com>
2023-09-23 17:36:40 +0200

    lavet lidt sql operationer

--- src/main/java/dao/MovieDAO.java
+++ src/main/java/dao/MovieDAO.java
@@ -1,7 +1,10 @@
 package dao;
 
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Query;
 import model.Movie;
+
+import java.util.List;
 
 public class MovieDAO {
 
@@ -20,7 +23,7 @@ public class MovieDAO {
         Movie movie = null;
 
         try (var em = emf.createEntityManager()) {
-           movie = em.find(Movie.class, id);
+            movie = em.find(Movie.class, id);
 
         }
         return movie;
@@ -38,23 +41,56 @@ public class MovieDAO {
     }
 
     public Movie updateMovie(Movie movie) {
-            try (var em = emf.createEntityManager()) {
-                em.getTransaction().begin();
-                em.merge(movie);
-                em.getTransaction().commit();
-            }
-            return movie;
+        try (var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.merge(movie);
+            em.getTransaction().commit();
+        }
+        return movie;
     }
 
     public Movie deleteMovie(Movie movie) {
-            try (var em = emf.createEntityManager()) {
-                em.getTransaction().begin();
-                em.remove(movie);
-                em.getTransaction().commit();
-            }
-            return movie;
+        try (var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            em.remove(movie);
+            em.getTransaction().commit();
+        }
+        return movie;
     }
 
+    //getAll
+    public Movie[] getAllMovies() {
+        Movie[] movies = null;
+        try (var em = emf.createEntityManager()) {
+            Query query = em.createQuery("SELECT m FROM Movie m");
+            return (Movie[]) query.getResultList().toArray(new Movie[0]);
+        }
+    }
+
+    //getByImdbId
+    public Movie getMovieByImdbId(String imdbId) {
+        Movie movie = null;
+        try (var em = emf.createEntityManager()) {
+            Query query = em.createQuery("SELECT m FROM Movie m WHERE m.imdbId = :imdbId");
+            query.setParameter("imdbId", imdbId);
+            return movie = (Movie) query.getSingleResult();
+        }
+    }
+    //getByRating
+    public List<Movie> getMoviesByRating(double rating) {
+        try (var em = emf.createEntityManager()) {
+            return em.createQuery("SELECT m FROM Movie m WHERE m.rating = :rating", Movie.class)
+                    .setParameter("rating", rating)
+                    .getResultList();
+        }
+    }
+
+    public List<Movie> getMoviesSortedByReleaseDate() {
+        try (var em = emf.createEntityManager()) {
+            return em.createQuery("SELECT m FROM Movie m ORDER BY m.releaseYear", Movie.class)
+                    .getResultList();
+        }
 
 
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import model.Movie;
 import utils.Scraper;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -13,15 +14,34 @@ public class Main {
 
         HibernateConfig.addAnnotatedClasses(Movie.class);
         var emf = HibernateConfig.getEntityManagerFactoryConfig("imdb_movies");
+            MovieDAO movieDAO = MovieDAO.getInstance(emf);
 
-        MovieDAO movieDAO = MovieDAO.getInstance(emf);
+        Movie[] movies = movieDAO.getAllMovies();
+        System.out.println("\nGet All Movies:");
+        for (Movie movie : movies) {
+            System.out.println(movie);
+        }
 
-        System.out.println("Scraping imdb top 250...");
+        System.out.println("\nGet Movie By Id: " + movieDAO.getMovieById(1));
+
+        System.out.println("\nGet Movie By Rating: ");
+        List<Movie> highRatedMovies = movieDAO.getMoviesByRating(8.0);
+        for (Movie movie : highRatedMovies) {
+            System.out.println(movie);
+        }
+
+        System.out.println("\nGet Movie By Release Year: ");
+        List<Movie> sortedMovieByRealsedate = movieDAO.getMoviesSortedByReleaseDate();
+        for (Movie movie : sortedMovieByRealsedate) {
+            System.out.println(movie);
+        }
+
+       /* System.out.println("Scraping imdb top 250...");
         List<Movie> movies = Scraper.fetchData(Movie.class);
 
         for (Movie m : movies) {
             movieDAO.saveMovie(m);
         }
-
+        System.out.println("Done scraping imdb top 250."); */
     }
 }
